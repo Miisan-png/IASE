4c89b2bbc8addb5f580e94c1d20425e7b1cc1b3b
Miisan <azarmisan@gmail.com>
2025-08-28 20:44:46 +0800

    reorg project

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from Engine.window_manager import WindowManager
+from Engine.scene_loader import SceneLoader
+from Game.player import Player
+from Game.camera import Camera
 
 def main():
     window = WindowManager()
+    scene = SceneLoader()
+    camera = Camera(window.width, window.height)
+    
+    scene.load_scene("Game/level1.json")
+    
+    player = Player(100, 100, scene)
+    camera.set_target(player)
+    
+    dt = 0
     
     while window.running:
+        dt = window.clock.tick(60) / 1000.0
+        
         window.handle_events()
+        
+        player.update(dt)
+        camera.update(dt)
+        
         window.clear_screen()
+        
+        scene.render(window.screen, camera.x, camera.y)
+        player.render(window.screen, camera.x, camera.y)
+        
         window.update_display()
     
     window.quit()
